b1fdf5e61833b223f739b5d13560bc38df0f8704
Michael Pretorius <michaelpretorius96@gmail.com>
2025-11-06 18:04:19 +0400

    feat: add commonjs support also, dual esm and commonjs support

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,28 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    lib: {
-      name: "matrix-crdt",
-      entry: resolve(__dirname, "src/index.ts"),
-      formats: ["es"],
-      fileName: () => `ixo-matrix-crdt.js`,
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: [
-        "yjs",
-        "vscode-lib",
-        "lib0",
-        "matrix-js-sdk",
-        "y-protocols",
-        "lodash",
-        "simple-peer",
-        "another-json",
-      ],
-      output: {
-        preserveModules: true,
-        exports: "named",
-      },
-    },
-  },
-  test: {
-    setupFiles: "src/setupTests.ts",
-    coverage: {
-      reporter: ["text", "json", "html", "lcov"],
-    },
-  },
+	build: {
+		lib: {
+			name: 'matrix-crdt',
+			entry: resolve(__dirname, 'src/index.ts'),
+			formats: ['cjs', 'es'],
+			fileName: format => `ixo-matrix-crdt.${format === 'es' ? 'mjs' : 'cjs'}`,
+		},
+		rollupOptions: {
+			// make sure to externalize deps that shouldn't be bundled
+			// into your library
+			external: ['yjs', 'vscode-lib', 'lib0', 'matrix-js-sdk', 'y-protocols', 'lodash', 'simple-peer', 'another-json'],
+			output: {
+				preserveModules: true,
+				exports: 'named',
+			},
+		},
+	},
+	test: {
+		setupFiles: 'src/setupTests.ts',
+		coverage: {
+			reporter: ['text', 'json', 'html', 'lcov'],
+		},
+	},
 });
